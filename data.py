acfb5953365acdf9e5e49d225f35288e931e6bea
sergsh1125-dotcom <serg.sh.1125@gmail.com>
2026-09-16 15:34:45 +0300

    Update data.py

--- data.py
+++ data.py
@@ -5,29 +5,29 @@
 
 RADII_DATA = {
     0.5: {
-        "trauma": {"severe": 0.20, "moderate": 0.35, "light": 0.55},
-        "burns": {"degree_3": 0.30, "degree_2": 0.45, "degree_1": 0.65},
-        "radiation": {"degree_4": 0.40, "degree_3": 0.50, "degree_2": 0.60, "degree_1": 0.75}
+        "trauma": {"severe": 0.22, "moderate": 0.32, "light": 0.55},
+        "burns": {"degree_3": 0.29, "degree_2": 0.36, "degree_1": 0.51},
+        "radiation": {"degree_4": 0.70, "degree_3": 0.79, "degree_2": 0.89, "degree_1": 1.00}
     },
     1.0: {
-        "trauma": {"severe": 0.25, "moderate": 0.45, "light": 0.70},
-        "burns": {"degree_3": 0.45, "degree_2": 0.65, "degree_1": 0.90},
-        "radiation": {"degree_4": 0.50, "degree_3": 0.65, "degree_2": 0.80, "degree_1": 1.00}
+        "trauma": {"severe": 0.27, "moderate": 0.40, "light": 0.69},
+        "burns": {"degree_3": 0.41, "degree_2": 0.50, "degree_1": 0.71},
+        "radiation": {"degree_4": 0.89, "degree_3": 0.94, "degree_2": 1.00, "degree_1": 1.12}
     },
     10.0: {
-        "trauma": {"severe": 0.55, "moderate": 0.95, "light": 1.50},
-        "burns": {"degree_3": 1.20, "degree_2": 1.60, "degree_1": 2.20},
-        "radiation": {"degree_4": 0.90, "degree_3": 1.10, "degree_2": 1.30, "degree_1": 1.60}
+        "trauma": {"severe": 0.59, "moderate": 0.86, "light": 1.49},
+        "burns": {"degree_3": 1.20, "degree_2": 1.50, "degree_1": 2.10},
+        "radiation": {"degree_4": 1.27, "degree_3": 1.33, "degree_2": 1.41, "degree_1": 1.55}
     },
     50.0: {
-        "trauma": {"severe": 1.00, "moderate": 1.70, "light": 2.60},
-        "burns": {"degree_3": 2.30, "degree_2": 3.10, "degree_1": 4.20},
-        "radiation": {"degree_4": 1.30, "degree_3": 1.50, "degree_2": 1.80, "degree_1": 2.20}
+        "trauma": {"severe": 1.00, "moderate": 1.46, "light": 2.54},
+        "burns": {"degree_3": 2.50, "degree_2": 3.10, "degree_1": 4.30},
+        "radiation": {"degree_4": 1.61, "degree_3": 1.64, "degree_2": 1.80, "degree_1": 1.96}
     },
     100.0: {
-        "trauma": {"severe": 1.25, "moderate": 2.15, "light": 3.30},
-        "burns": {"degree_3": 3.10, "degree_2": 4.10, "degree_1": 5.60},
-        "radiation": {"degree_4": 1.50, "degree_3": 1.80, "degree_2": 2.10, "degree_1": 2.50}
+        "trauma": {"severe": 1.27, "moderate": 1.84, "light": 3.20},
+        "burns": {"degree_3": 3.50, "degree_2": 4.20, "degree_1": 5.80},
+        "radiation": {"degree_4": 1.69, "degree_3": 1.83, "degree_2": 1.99, "degree_1": 2.16}
     }
 }
 
